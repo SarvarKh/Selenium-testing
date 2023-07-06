265072a0ba33d22dfb94bef35e7054d60688f36e
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-07-06 15:43:26 +0500

    Add  dependencies to pom

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.*;
+
 public class SimpleTests {
+    @BeforeAll
+    static void beforeClassSetup() {
+        System.out.println("Preparing test class");
+    }
+
+    @BeforeEach
+    void prepareTest() {
+        System.out.println("@BeforeEach is calling prepareTest()");
+    }
+
+    @Test
+    void testSimpleTest() {
+        System.out.println("Test is running");
+    }
+
+
+    @Test
+    void testSimpleTest2() {
+        System.out.println("Test 2 is running");
+    }
+
+    @AfterEach
+    void clearTestData() {
+        System.out.println("Clearing up test data");
+    }
+
+    @AfterAll
+    static void afterClassTearDown() {
+        System.out.println("Tear down test class");
+    }
 }
